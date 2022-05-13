ff379d6ac1ecf86fe55fc838aec6811e8c8bf436
gabrielalencar <gabrielalencar33564@gmail.com>
2022-05-13 18:20:12 -0300

    Hello, Worldgit status

---
+++ script.js
@@ -0,0 +1,9 @@
+const body = document.querySelector('body');
+
+
+const main = document.createElement('main');
+body.appendChild(main);
+
+const p = document.createElement('p');
+p.innerText = 'Hello, World!'
+main.appendChild(p)
